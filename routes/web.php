653ab1df43e5e3cbd06f61f0d11fdf813f6cc5ac
ThawZin999 <egthawzin@gmail.com>
2024-03-06 11:39:36 +0630

    redirect index to users route

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -29,6 +27,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('/', function () {
+        return redirect('users');
+    });
     Route::resources([
         'users' => UserController::class,
         'roles' => RoleController::class,
