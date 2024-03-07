899db3bdc4972f70c84783d8b97b2ad663aeb22a
ThawZin999 <egthawzin@gmail.com>
2024-03-07 08:38:46 +0630

    updated permissions style to pill group

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -28,8 +28,8 @@
                     </div>
                     {{-- Table --}}
                     <div class="overflow-x-auto">
-                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                        <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs  text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th scope="col" class="px-6 py-4">Role Name</th>
                                     <th scope="col" class="px-6 py-4">Permission</th>
@@ -47,12 +47,12 @@
                                             {{ $r->name }}
                                         </th>
 
-                                        <td class="px-6 py-6">
-                                            <ul>
-                                                @foreach ($r->permissions as $p)
-                                                    <li class="text-blue-500">{{ $p->name }}</li>
-                                                @endforeach
-                                            </ul>
+                                        <td class="px-6 py-4 flex flex-wrap ">
+                                            @foreach ($r->permissions as $p)
+                                                <div class=" px-2 m-1  text-white bg-blue-300 rounded-full">
+                                                    {{ $p->name }}
+                                                </div>
+                                            @endforeach
                                         </td>
                                         <td class="px-6 py-4">
                                             @canany(['edit', 'delete'], auth()->user())
